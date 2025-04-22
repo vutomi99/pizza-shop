67f088b3b0b0aef8dabe62e9297286db423f980d
vutomi99 <vutomi99.ngobeni@gmail.com>
2025-04-22 15:44:10 +0200

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../app/styles/Navbar.module.css";
 
+
+import HomeIcon from "@mui/icons-material/Home";
+import LocalPizzaIcon from "@mui/icons-material/LocalPizza";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import LoginIcon from "@mui/icons-material/Login";
+
 const Navbar = () => {
   return (
     <div className={styles.container}>
+     
       <div className={styles.item}>
         <div className={styles.callButton}>
           <Image src="/image/telephone.png" alt="" width={32} height={32} />
@@ -14,18 +23,43 @@ const Navbar = () => {
           <div className={styles.text}>079 700 9081</div>
         </div>
       </div>
+
+     
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
-          <li className={styles.listItem}>Products</li>
-          <li className={styles.listItem}>Menu</li>
-          <Image src="/image/logo.png" alt="" width={160} height={69} />
-          <Link href="/auth/login">
-            <li className={styles.listItem}>Login</li>
+          <Link href="/" passHref>
+            <li className={styles.listItem}>
+              <HomeIcon style={{ marginRight: "5px" }} />
+              Home
+            </li>
           </Link>
-          <li className={styles.listItem}>Contact</li>
+
+          <Link href="/products" passHref>
+            <li className={styles.listItem}>
+              <LocalPizzaIcon style={{ marginRight: "5px" }} />
+              Products
+            </li>
+          </Link>
+
+          <Link href="/menu" passHref>
+            <li className={styles.listItem}>
+              <RestaurantMenuIcon style={{ marginRight: "5px" }} />
+              Menu
+            </li>
+          </Link>
+
+          <Image src="/image/logo.png" alt="" width={160} height={69} />
+
+          <Link href="/auth/login" passHref>
+            <li className={styles.listItem}>
+              <LoginIcon style={{ marginRight: "5px" }} />
+              Login
+            </li>
+          </Link>
         </ul>
       </div>
+
+      {/* Right Section: Cart */}
       <div className={styles.item}>
         <div className={styles.cart}>
           <Image src="/image/cart.png" alt="" width={30} height={30} />
