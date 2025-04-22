edac32a00eeb97c95dfc90d63a63e266a5e3b351
vutomi99 <vutomi99.ngobeni@gmail.com>
2025-04-22 02:59:43 +0200

    navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import styles from "../app/styles/Navbar.module.css";
+
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/image/telephone.png" alt="" width={32} height={32} />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>079 700 9081</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/image/logo.png" alt="" width={160} height={69} />
+          <li className={styles.listItem}>Login</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/image/cart.png" alt="" width={30} height= {30} />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
