565d16c8cbec50a40fd88f2886bea3fbfaa80e7c
vutomi99 <vutomi99.ngobeni@gmail.com>
2025-04-25 09:03:47 +0200

    updated  wrapping

--- src/components/app/layout/index.jsx
+++ src/components/app/layout/index.jsx
@@ -3,13 +3,14 @@
 import Navbar from "@/components/Navbar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import { CartProvider } from "@/hooks/cart";
 
 const AppLayoutComponent = ({ children }) => {
   const client = new QueryClient({});
   return (
     <QueryClientProvider client={client}>
       <Navbar />
-      {children}
+      <CartProvider>{children}</CartProvider>
     </QueryClientProvider>
   );
 };
